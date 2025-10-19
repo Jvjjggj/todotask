9fce877fed8f6e63f0b4cff1eec80c423a1ecfd4
Jakeer <sjakeerhussain244@gmail.com>
2025-10-20 00:07:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,136 @@
+const express = require("express")
+const sqlite3 = require("sqlite3")
+const { open } = require("sqlite")
+const path = require("path")
+const app = express()
+
+app.use(express.json())
+
+const dbPath = path.join(__dirname, "tasks.db")
+
+let db = null;
+
+const connectToServer = async () => {
+    try {
+        db = await open(
+            {
+                filename: dbPath,
+                driver: sqlite3.Database
+            }
+        )
+
+        db.run(
+            ` 
+          create table if not exists employee(
+            id interger primary key,
+            salary text
+          )
+        `
+        )
+        app.listen(4000, () => {
+            console.log("App running success")
+        })
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+app.get("/task/sort",async(req,res)=>{
+    const query=`select * from tasks`
+    const response=await db.all(query)
+
+    response.sort((a,b)=>{
+        const order={"High":1,"Medium":2,"Low":3}
+
+        if (order[a.priority]!==order[b.priority])
+        {
+            return order[a.priority]-order[b.priority]
+        }
+    })
+
+    res.json(response)
+})
+
+app.get("/tasks", async (req, res) => {
+    const query = `
+    select * from tasks
+    `
+    const response = await db.all(query)
+
+    res.json(response)
+})
+
+app.post("/task", async (req, res) => {
+    const { title, priority, dueDate } = req.body
+    const createdAt = new Date().toISOString()
+    const query = `insert into tasks(title,priority,dueDate,createdAt)
+    values("${title}","${priority}","${dueDate}","${createdAt}")`
+
+    await db.run(query)
+
+    res.json("Task")
+})
+
+app.put("/task/:id", async (req, res) => {
+    let { id } = req.params
+    const { title, priority, dueDate } = req.body
+    const query = `
+    select * from tasks
+    `
+    id = parseInt(id)
+    const response = await db.all(query)
+    const taskwithId = response.filter(i => i.id === id)
+    if (!taskwithId) {
+        res.status(400).json("Task id not found")
+    }
+    const createdAt = new Date().toISOString();
+    const query1 =
+        `
+    update tasks
+    set 
+    title="${title}",
+    priority="${priority}",
+    dueDate="${dueDate}",
+    createdAt="${createdAt}"
+
+    where id=${id}
+    `
+    db.run(query1);
+    res.json("Task updated successfully")
+
+})
+
+app.get("/task/:id",async(req,res)=>{
+    let {id} =req.params
+    id = parseInt(id)
+
+    const query=`select * from tasks`
+    const response=await db.all(query)
+    const taskById=response.filter(i=>i.id===id)
+    if (taskById.length===0)
+    {
+        res.status(400).json("Task not found")
+    }
+    console.log(taskById)
+    res.json(taskById)
+})
+
+app.delete("/task/:id",async(req,res)=>{
+    let {id}=req.params
+    id=parseInt(id)
+    const query=
+    `
+      delete from tasks where id=${id}
+    `
+
+    await db.run(query)
+    res.json("Task deleted")
+})
+
+
+
+connectToServer()
